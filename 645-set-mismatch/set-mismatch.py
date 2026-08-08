d753f70769976bf8025850abf0e695f7f2b757f0
HoneyComb <rahulrajrai071845@gmail.com>
2026-08-09 03:38:03 +0530

    Time: 12 ms (56.89%) | Memory: 21 MB (27.31%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findErrorNums(self, nums):
+        seen = set()
+        duplicate = 0
+
+        for num in nums:
+            if num in seen:
+                duplicate = num
+            else:
+                seen.add(num)
+
+        for i in range(1, len(nums) + 1):
+            if i not in seen:
+                missing = i
+                break
+
+        return [duplicate, missing]
