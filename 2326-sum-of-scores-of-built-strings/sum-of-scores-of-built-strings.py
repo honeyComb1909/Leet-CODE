c88b981553e84a8e7b3fdea2f3da603d315366b0
Rahul Raj Rai <rahulrajrai071845@gmail.com>
2026-08-10 07:06:19 +0530

    Time: 275 ms (50.54%) | Memory: 23.7 MB (26.09%) - LeetSync

---
+++ 2326-sum-of-scores-of-built-strings/sum-of-scores-of-built-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sumScores(self, s: str) -> int:
+        n = len(s)
+        z = [0] * n
+
+        left = right = 0
+
+        for i in range(1, n):
+            if i <= right:
+                z[i] = min(right - i + 1, z[i - left])
+
+            while i + z[i] < n and s[z[i]] == s[i + z[i]]:
+                z[i] += 1
+
+            if i + z[i] - 1 > right:
+                left = i
+                right = i + z[i] - 1
+
+        return n + sum(z)
